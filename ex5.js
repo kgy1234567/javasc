9614c395c9e83d2ad9c16d5afd6790c82f2e24b2
Geunyeong Kim <kgy1234567@naver.com>
2020-01-02 17:52:52 +0900

    message

--- ex5.js
+++ ex5.js
@@ -1,1 +1,20 @@
-let a,b;
+let a,b,k,str1,str2;
+
+for(a=100;a<1000;a++){
+    for(b=100;b<1000;b++){
+        let k=a*b;
+         str1= String(k);
+        
+        let str2=str1.split('').reverse().join('');
+
+        if(str2===str1){
+            console.log(str2);
+        }
+         
+
+
+    }
+}
+
+
+
---
+++ ex6.js
@@ -0,0 +1,11 @@
+
+let a=12;
+let b=14;
+
+let k=a*b;
+
+
+         k = String(k);
+        k=k.split("");
+        let reversek=k.reverse();
+        console.log(reversek);
